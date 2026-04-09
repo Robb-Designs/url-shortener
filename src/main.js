924ccb5abe4c947501411b1977c2662003f4f66a
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-09 14:07:15 -0400

    Testing: created logic for hanling submission of the url with validation

---
+++ src/main.js
@@ -0,0 +1,43 @@
+
+
+// DOM VARIABLES -----------------------------------------------------------------------------
+// Form elements
+const shortenForm = document.getElementById('shorten-form');
+const urlInput = document.getElementById('url-input');
+const submitBtn = document.getElementById('submit-btn');
+
+//states & messages
+const errorMessage = document.getElementById('error-message');
+const loading = document.getElementById('loading');
+const result = document.getElementById('result');
+
+//result elements
+const shortUrl = document.getElementById('short-url');
+
+//copy button
+const copyBtn = document.getElementById('copy-btn');
+
+
+//console.log(urlInput, submitBtn, errorMessage, loading, result, shortUrl, copyBtn);
+
+
+// Functions ----------------------------------------------------------------------------------------
+ // gets url from input and validates it
+
+ function handleSubmit(e) {
+    e.preventDefault(); // Prevent form from normal defaults
+    const url = urlInput.value.trim(); // .trim() to remove any whitespace
+
+    // Basic validation: check if the input is not empty and is a valid URL
+    if (!url) {
+        displayError('Please enter a URL.');
+        return;
+    }
+
+    //console.log('Submitted URL:', url);
+ }
+
+
+
+ // Event Listeners ----------------------------------------------------------------------------------
+ shortenForm.addEventListener('submit', handleSubmit);
