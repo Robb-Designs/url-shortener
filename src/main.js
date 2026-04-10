cc47eac901fbefd0314edb0874680d4ec2296176
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-10 13:29:00 -0400

    fixed fetching logic in main.js to get data from bitly api

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 // IMPORTS ------------------------------------------------------------------------------------------
 import { showLoading } from './ui.js';
+import { hideLoading } from './ui.js';
 import { showResult } from './ui.js';
 import { validateUrl } from './utils.js';
+import { fetchShortUrl } from './api.js';
 
 // DOM VARIABLES -----------------------------------------------------------------------------
 // Form elements
@@ -11,7 +13,6 @@ const submitBtn = document.getElementById('submit-btn');
 
 //states & messages
 const errorMessage = document.getElementById('error-message');
-const loading = document.getElementById('loading');
 const result = document.getElementById('result');
 
 //result elements
@@ -25,9 +26,9 @@ const copyBtn = document.getElementById('copy-btn');
 
 
 // Functions ----------------------------------------------------------------------------------------
- // gets url from input and validates it
+// gets url from input and validates it
 
- async function handleSubmit(e) {
+async function handleSubmit(e) {
     e.preventDefault(); // Prevent form from normal defaults
     let url = urlInput.value.trim(); // .trim() to remove any whitespace
 
@@ -35,18 +36,24 @@ const copyBtn = document.getElementById('copy-btn');
 
     // imported validation function from utils.js
     try {
-        validateUrl(url); // Validate the URL format
+        url = validateUrl(url); // Validate the URL format and declaring url as the validated URL returned from the function.
         showLoading(submitBtn, urlInput); // Show loading state while validating
-    
+        //this line simulates loading using setTimeout.
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        const shortLink = await fetchShortUrl(url); // Fetch the shortened URL from the API
+        console.log(shortLink);
+
     } catch (error) {
         console.error(error.message);
         return;
+    } finally {
+        hideLoading(submitBtn, urlInput); // Hide loading state after validation and API call
     }
 
     console.log('Submitted URL:', url);
- }
+}
 
 
 
- // Event Listeners ----------------------------------------------------------------------------------
- shortenForm.addEventListener('submit', handleSubmit);
+// Event Listeners ----------------------------------------------------------------------------------
+shortenForm.addEventListener('submit', handleSubmit);
