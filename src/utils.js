316fd39ee21b1224b871c4cb061c9e0a065d6d28
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-10 10:37:34 -0400

    refactor: made stricter validations for user input of their url

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,19 @@
 
 
 export function validateUrl(url) {
+    // checking if input is empty or not a url
     if (!url) {
         throw new Error('Please enter a URL.');
     }
 
+    // adding http:// if the user didntt include it
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+        url = 'http://' + url;
+    }
+
     try {
         new URL(url); // This will throw if the URL is invalid
+        return url; 
     } catch {
         throw new Error('Invalid URL format. (include http:// or https://)');
     }
